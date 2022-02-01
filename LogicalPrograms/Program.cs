60fb4a9222a339892cd0cd2e1071fcc8627071b1
saifalishaikh31 <saifalishaikh31>
2022-02-01 15:48:12 +0530

    [Saif Ali] Add: Simulate StopWatch Program Added

--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -17,7 +17,8 @@ namespace LogicalPrograms
                                  + "\n 3.Prime Number."
                                  + "\n 4.Reverse A Number."
                                  + "\n 5.Generate Coupon Numbers."
-                                 + "\n 6.Exit \n");
+                                 + "\n 6.Simulate Stop Watch."
+                                 + "\n 7.Exit \n");
                  int option = Convert.ToInt32(Console.ReadLine());
                  switch (option)
                  {
@@ -42,6 +43,10 @@ namespace LogicalPrograms
                          couponnum.GenerateCoupunNumbers();
                          break;
                      case 6:
+                         SimulateStopWatch stopwatch = new SimulateStopWatch();
+                         stopwatch.StopWatch();
+                         break;
+                     case 7:
                          flag = false;
                          break;
                      default: Console.WriteLine("Choose Correct option");
---
+++ LogicalPrograms/SimulateStopWatch.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LogicalPrograms
+{
+    class SimulateStopWatch
+    {
+        public void StopWatch()
+        {
+
+            int startTimeMilliseconds = 0, startTimeSeconds = 0, startTimeMinutes = 0, startTimeHours = 0; 
+            int endTimeMilliseconds = 0, endTimeSeconds = 0, endTimeMinutes = 0, endTimeHours = 0;
+            int elapsedMilliseconds = 0, elapsedSeconds = 0, elapsedMinutes = 0, elapsedHours = 0;
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("\nEnter 1 to Start Stopwatch"
+                                 +"\nEnter 2 to Stop Stopwatch");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine("\nStopwatch has Started!");
+                        DateTime dtStart = DateTime.Now;
+                        startTimeMilliseconds = dtStart.Millisecond;
+                        startTimeSeconds = dtStart.Second;
+                        startTimeMinutes = dtStart.Minute;
+                        startTimeHours = dtStart.Hour;
+                        Console.WriteLine("\nStart Time of StopWatch : {0}:{1}:{2}:{3} ", startTimeHours, startTimeMinutes, startTimeSeconds, startTimeMilliseconds);
+                    break;
+
+                    case 2:
+                        Console.WriteLine("\nStopwatch has Stop!");
+                        DateTime dtEnd = DateTime.Now;
+                        endTimeMilliseconds = dtEnd.Millisecond;
+                        endTimeSeconds = dtEnd.Second;
+                        endTimeMinutes = dtEnd.Minute;
+                        endTimeHours = dtEnd.Hour;
+                        Console.WriteLine("\nStart Time of StopWatch : {0}:{1}:{2}:{3} ", endTimeHours, endTimeMinutes, endTimeSeconds, endTimeMilliseconds);
+
+                        elapsedMilliseconds = endTimeMilliseconds - startTimeMilliseconds;
+                        elapsedSeconds = endTimeSeconds - startTimeSeconds;
+                        elapsedMinutes = endTimeMinutes - startTimeMinutes;
+                        elapsedHours = endTimeHours - startTimeHours;
+                        Console.WriteLine("\nTime Elapsed is : {0}:{1}:{2}:{3}", elapsedHours, elapsedMinutes, elapsedSeconds, elapsedMilliseconds);
+                    break;
+
+                    case 3:
+                        flag = false;
+                    break;
+
+                    default:
+                        Console.WriteLine("Choose Correct Option!!!\n");
+                    break;
+                }
+            }
+
+        }
+    }
+}
